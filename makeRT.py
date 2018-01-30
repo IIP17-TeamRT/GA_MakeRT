9b5d35f9294bbb792e44a955e53c8582722b32ca
YaGiNA <yutachihaya@gmail.com>
2018-01-30 14:20:53 +0900

    Make function to output graph of average of intersection

--- makeRT.py
+++ makeRT.py
@@ -6,28 +6,6 @@ from deap import base
 from deap import creator
 from deap import tools
 
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMax)
-
-img_ans = plt.imread('TAsample2.png')
-img_ans = img_ans[..., 1]
-img_ans = img_ans.reshape(40*30, )
-img_ans = img_ans*255
-
-for i in range(40*30):
-    if img_ans[i] < 128:
-        img_ans[i] = 0
-    else:
-        img_ans[i] = 1
-
-toolbox = base.Toolbox()
-toolbox.register("attr_bool", random.randint, 0, 1)
-toolbox.register(
-    "individual", tools.initRepeat, creator.Individual,
-    toolbox.attr_bool, 40*30
-)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-
 
 def evalOneMax(individual):
     arrayind = np.array(individual)
@@ -37,14 +15,8 @@ def evalOneMax(individual):
     return hitpoint,
 
 
-toolbox.register("evaluate", evalOneMax)
-toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
-toolbox.register("select", tools.selTournament, tournsize=3)
-
-
-def main():
-    avgs = np.array([])
+def doGT(whichcx):
+    toolbox.register("mate", whichcx)
     random.seed(64)
     pop = toolbox.population(n=300)
     CXPB, MUTPB, NGEN = 0.5, 0.2, 200
@@ -56,7 +28,7 @@ def main():
         ind.fitness.values = fit
 
     print(" Evaluate %i individuals" % len(pop))
-
+    avgs = np.zeros(NGEN)
     for g in range(NGEN):
         print("--Generation %i --" % g)
 
@@ -93,7 +65,7 @@ def main():
         print(" Max %s" % max(fits))
         print(" Avg %s" % mean)
         print(" std %s" % std)
-        avgs = np.append(avgs, mean)
+        avgs[g] = mean
 
     print("-- End of (successful) evolution --")
     best_ind = tools.selBest(pop, 1)[0]
@@ -101,11 +73,38 @@ def main():
 
     best_ind_img = np.array(best_ind)
     best_ind_img = best_ind_img.reshape((40, 30))
-
-    # plt.imshow(best_ind_img, cmap='gray')
     plt.plot(avgs)
+
+
+def main():
+    cxs = [tools.cxTwoPoint, tools.cxOnePoint]
+    [doGT(cx) for cx in cxs]
     plt.show()
 
 
 if __name__  == "__main__":
+    creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+    creator.create("Individual", list, fitness=creator.FitnessMax)
+
+    img_ans = plt.imread('TAsample2.png')
+    img_ans = img_ans[..., 1]
+    img_ans = img_ans.reshape(40*30, )
+    img_ans = img_ans*255
+
+    for i in range(40*30):
+        if img_ans[i] < 128:
+            img_ans[i] = 0
+        else:
+            img_ans[i] = 1
+
+    toolbox = base.Toolbox()
+    toolbox.register("attr_bool", random.randint, 0, 1)
+    toolbox.register(
+        "individual", tools.initRepeat, creator.Individual,
+        toolbox.attr_bool, 40*30
+    )
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    toolbox.register("evaluate", evalOneMax)
+    toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+    toolbox.register("select", tools.selTournament, tournsize=3)
     main()
